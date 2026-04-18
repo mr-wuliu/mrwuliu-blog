fc73522f01e4382010149a907227a64d7e2f5b90
mrwuliu <mrwuliu@MrWuliu.lan>
2026-04-18 17:43:48 +0800

    build: rebuild admin assets
    Rebuild admin SPA after collections feature changes.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ public/admin/assets/channel-Ds702WyR.js
@@ -0,0 +1,1 @@
+import{aq as o,ar as n}from"./index-CB_0nbwP.js";const t=(r,a)=>o.lang.round(n.parse(r)[a]);export{t as c};
---
+++ public/admin/assets/stateDiagram-v2-QKLJ7IA2-BabTtNjo.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-OYMX7WX6-BtAg_Frv.js";import{_ as i}from"./index-CB_0nbwP.js";import"./chunk-55IACEB6-ByPKfWfr.js";import"./chunk-EDXVE4YY-DMlbAws2.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
